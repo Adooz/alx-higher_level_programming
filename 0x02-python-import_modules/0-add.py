9dffe49b4d66386384888d3c215e41f6b9acd7f3
Adooz <kndonake@gmail.com>
2023-05-11 08:14:17 -0700

    update on 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -5,6 +5,4 @@ if __name__ == "__main__":
     a = 1
     b = 2
 
-    add_func = add(a, b)
-
-    print("{:d} + {:d} = {:d}".format(a, b, add_func))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b))
