f791ca24cdc97bd2f6ab42e3043966e15be1fade
Adooz <kndonake@gmail.com>
2023-08-15 10:19:11 -0700

    create 3-rectangle.js

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,18 +1,17 @@
 #!/usr/bin/node
 
 module.exports = class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
   }
 
-  print() {
+  print () {
     const symbol = 'X'.repeat(this.width);
     for (let i = 0; i < this.height; i++) {
       console.log(symbol);
     }
   }
 };
-}
