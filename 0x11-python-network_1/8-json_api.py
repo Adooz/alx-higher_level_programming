92e39ca1ef6039f9fc9eb868b38118f3cbf25b69
Adooz <kndonake@gmail.com>
2023-09-01 19:23:15 +0100

    added 8-json_api.py

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""takes in a letter and sends a POST request to 
+http://0.0.0.0:5000/search_user with the letter as a
+parameter. The letter must be sent in the variable q
+If no argument is given, set q=""
+If the response body is properly JSON formatted and
+not empty, display the id and name like 
+this: [<id>] <name> Otherwise:
+Display Not a valid JSON if the JSON is invalid
+Display No result if the JSON is empty
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        q = ""
+    else:
+        q = sys.argv[1]
+    r = requests.post("http://0.0.0.0:5000/search_user", data={"q": q})
+    if r.status_code == 200:
+        if r.text != "":
+            try:
+                data = r.json()
+                print("[{}] {}".format(data["id"], data["name"]))
+            except ValueError:
+                print("Not a valid JSON")
+        else:
+            print("No result")
+    else:
+        print("Error: {}".format(r.status_code))
