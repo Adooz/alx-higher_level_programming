d4fb1224fc6ba9221c5b6b0bf01e0aa4ffa6b054
Adooz <kndonake@gmail.com>
2023-09-01 16:13:02 +0100

    fixed pycodestyle

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,7 +6,7 @@ import requests
 if __name__ == "__main__":
     response = requests.get('https://alx-intranet.hbtn.io/status')
     if response.status_code == 200:
-        # If the request was successful, you can access the content of the response
+        #If the request was successful, you can access the content of the response
         content = response.text
         print("Body response:")
         print(f"\t- type: {type(content)}")
