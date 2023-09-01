2f36ae03eb03e38ad0c2ca87e7f0cf314571f032
Adooz <kndonake@gmail.com>
2023-09-01 15:14:15 +0100

    fixing syntax

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -4,14 +4,14 @@ passed URL with the email as a parameter,
 and displays the body of the response
 """
 from sys import argv
-from urllib import parse
-from urllib import request
+import urllib.parse
+import urllib.request
 
 if __name__ == "__main__":
     url = argv[1]
     value = {'email': argv[2]}
-    data = parse.urlencode(value).encode('ascii')
-    request = request.Request(url, data=data)
-    with request.urlopen() as response:
+    data = urllib.parse.urlencode(value).encode('ascii')
+    req = urllib.request.Request(url, data=data)
+    with urllib.request.urlopen(req) as response:
         body = response.read().decode('utf-8')
         print(body)
