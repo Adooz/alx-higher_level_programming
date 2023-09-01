0f59013034860942a7cf3ab697de83cd40489af6
Adooz <kndonake@gmail.com>
2023-09-01 14:54:34 +0100

    added 2-post_email.py

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" takes in a URL and an email, sends a POST request to the
+passed URL with the email as a parameter,
+and displays the body of the response
+"""
+from sys import argv
+from urllib import request
+
+if __name__ == "__main__":
+    url = argv[1]
+    email = argv[2]
+    data = {'email': email}
+    with request.post(url, data=data) as response:
+        if response.status == 200:
+            print(f"Your email is: {email}")
