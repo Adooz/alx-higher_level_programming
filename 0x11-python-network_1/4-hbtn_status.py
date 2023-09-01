b5af4b39cbef584436c7a56270e8481203fa60fa
Adooz <kndonake@gmail.com>
2023-09-01 16:11:10 +0100

    fixed pycodestyle

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,14 +2,15 @@
 """ fetches https://alx-intranet.hbtn.io/status using requests"""
 import requests
 
-response = requests.get('https://alx-intranet.hbtn.io/status')
-    
-if response.status_code == 200:
-    # If the request was successful, you can access the content of the response
-    content = response.text
-    print("Body response:")
-    print(f"\t- type: {type(content)}")
-    print(f"\t- content: {content}")
-else:
-    print("Error:")
-    print(f"\t- status code: {response.status_code}")
+
+if __name__ == "__main__":
+    response = requests.get('https://alx-intranet.hbtn.io/status')
+    if response.status_code == 200:
+        # If the request was successful, you can access the content of the response
+        content = response.text
+        print("Body response:")
+        print(f"\t- type: {type(content)}")
+        print(f"\t- content: {content}")
+    else:
+        print("Error:")
+        print(f"\t- status code: {response.status_code}")
