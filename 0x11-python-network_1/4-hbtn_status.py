b73f236b49d5899718a5816722c784ab2e607e25
Adooz <kndonake@gmail.com>
2023-09-01 16:05:36 +0100

    fixed syntax

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,12 +4,12 @@ import requests
 
 response = requests.get('https://alx-intranet.hbtn.io/status')
     
-    if response.status_code == 200:
-        # If the request was successful, you can access the content of the response
-        content = response.text
-        print("Body response:")
-        print(f"\t- type: {type(content)}")
-        print(f"\t- content: {content.decode(utf-8)}")
-    else:
-        print("Error:")
-        print(f"\t- status code: {response.status_code}")
+if response.status_code == 200:
+    # If the request was successful, you can access the content of the response
+    content = response.text
+    print("Body response:")
+    print(f"\t- type: {type(content)}")
+    print(f"\t- content: {content.decode(utf-8)}")
+else:
+    print("Error:")
+    print(f"\t- status code: {response.status_code}")
