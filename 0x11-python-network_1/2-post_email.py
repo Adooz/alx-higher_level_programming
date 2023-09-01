b6828955f9845066f31d245c61e86a27e0595400
Adooz <kndonake@gmail.com>
2023-09-01 14:59:44 +0100

    fixing syntax

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -4,12 +4,12 @@ passed URL with the email as a parameter,
 and displays the body of the response
 """
 from sys import argv
-from urllib import request
+from urllib import requests
 
 if __name__ == "__main__":
     url = argv[1]
     email = argv[2]
     data = {'email': email}
-    with request.post(url, data=data) as response:
+    with requests.post(url, data=data) as response:
         if response.status == 200:
             print(f"Your email is: {email}")
