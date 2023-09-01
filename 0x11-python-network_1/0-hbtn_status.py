226578304a6fcca1e26b765048c7a1394bfb2b88
Adooz <kndonake@gmail.com>
2023-09-01 14:03:38 +0100

    added 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""a Python script that fetches https://alx-intranet.hbtn.io/status """
+""" fetches https://alx-intranet.hbtn.io/status """
 from urllib import request
 
 
@@ -7,6 +7,6 @@ if __name__ == "__main__":
     with request.urlopen('https://alx-intranet.hbtn.io/status') as response:
         content = response.read()
     print("Body response:")
-    print("\t - type: {type(content)}")
-    print("\t - content: {content}")
-    print("\t - utf8 content: {content.decode('utf-8')}")
+    print("\t- type: {type(content)}")
+    print("\t- content: {content}")
+    print("\t- utf8 content: {content.decode('utf-8')}")
