097b94149a61345c9a3e831a3b183c490db6ee32
Adooz <kndonake@gmail.com>
2023-09-01 16:48:40 +0100

    fixed last check

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -10,6 +10,6 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
-    payload = {"email": email}
-    r = requests.post(url, params=payload)
+    value = {'email': email}
+    r = requests.post(url, data=value)
     print(r.text)
