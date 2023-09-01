5bafea2d8f7a7f5482573d4d08c6d8f8f7f9b98a
Adooz <kndonake@gmail.com>
2023-09-01 19:46:04 +0100

    fixed pycodestyle

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""takes in a letter and sends a POST request to 
+"""takes in a letter and sends a POST request to
 http://0.0.0.0:5000/search_user with the letter as a
 parameter. The letter must be sent in the variable q
 If no argument is given, set q=""
 If the response body is properly JSON formatted and
-not empty, display the id and name like 
+not empty, display the id and name like
 this: [<id>] <name> Otherwise:
 Display Not a valid JSON if the JSON is invalid
 Display No result if the JSON is empty
@@ -14,7 +14,7 @@ import sys
 
 
 if __name__ == "__main__":
-    q = "" if len(sys.argv) == 1 else sys.argv[1]    
+    q = "" if len(sys.argv) == 1 else sys.argv[1]
     r = requests.post("http://0.0.0.0:5000/search_user", data={"q": q})
     try:
         data = r.json()
@@ -23,5 +23,4 @@ if __name__ == "__main__":
         else:
             print("No result")
     except ValueError:
-            print("Not a valid JSON")
-
+        print("Not a valid JSON")
