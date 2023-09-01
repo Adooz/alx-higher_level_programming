5355c66f0d1c782a397135e69830955de9c4570f
Adooz <kndonake@gmail.com>
2023-09-01 17:05:17 +0100

    fixed pycodestyle

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" takes in a URL, sends a request to the URL and displays 
+"""takes in a URL, sends a request to the URL and displays
 the body of the response.
 If the HTTP status code is greater than or equal to 400,
 print: Error code: followed by the value of
