8324bf78d603643e84894b9a9b491506d91d9433
Adooz <kndonake@gmail.com>
2023-09-01 19:43:29 +0100

    added 8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -14,7 +14,7 @@ import sys
 
 
 if __name__ == "__main__":
-    q = "" if len(sys.argv) == 1 else sys.argv[1]:    
+    q = "" if len(sys.argv) == 1 else sys.argv[1]    
     r = requests.post("http://0.0.0.0:5000/search_user", data={"q": q})
     try:
         data = r.json()
